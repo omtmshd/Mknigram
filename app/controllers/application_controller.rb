1e1379290ac6cbdea8dc0be143dfd664e235e8cc
omtmshd <67169292+omtmshd@users.noreply.github.com>
2020-07-25 06:17:35 +0900

    refactor: add background image (#40)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  $category_l1 = Category.where(ancestry: nil)
-
   protected
 
   def configure_permitted_parameters
