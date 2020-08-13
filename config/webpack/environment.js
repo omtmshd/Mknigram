93c5f6bbb00df26407db1010a68781b2f71b71c9
omtmshd <omt1031mshd@gmail.com>
2020-08-14 02:26:40 +0900

    refactor: remove bulma-rails,jquery

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,13 +2,6 @@ const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
 
-const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
-  })
-)
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
 module.exports = environment
