e8ebec0cec553aa96c8d6ef61280b7c80944ef11
omtmshd <67169292+omtmshd@users.noreply.github.com>
2020-09-02 16:29:13 +0900

    feat: いいね一覧表示 (#73)
    * feat: devise_token_auth 導入
    
    * refactor: いいね一覧表示

--- app/javascript/pages/users/UsersShowPage.vue
+++ app/javascript/pages/users/UsersShowPage.vue
@@ -1,21 +1,38 @@
 <template>
   <div>
-    <user-show :user="user" @set-follow="updateUser()"></user-show>
-    <posts-index :posts-data="user.posts" @update-posts="updateUser()"></posts-index>
+    <v-card>
+      <user-show :user="user" @set-follow="updateUser()"></user-show>
+      <template>
+        <v-tabs v-model="tabsKey" centered color="#43A047">
+          <v-tab v-for="t in tabasItem" :key="t.index">{{ t }}</v-tab>
+        </v-tabs>
+      </template>
+
+      <v-tabs-items v-model="tabsKey">
+        <v-tab-item>
+          <v-card-text v-if="user.posts.length === 0" class="subtitle-2 text-center">投稿はありません</v-card-text>
+
+          <posts-index-image v-else :posts-data="user.posts" @update-posts="updateUser()"></posts-index-image>
+        </v-tab-item>
+        <v-tab-item>
+          <v-card-text v-if="likesPosts.length === 0" class="subtitle-2 text-center">いいねした投稿はありません</v-card-text>
+
+          <posts-index-image v-else :posts-data="likesPosts" @update-posts="updateLikes()"></posts-index-image>
+        </v-tab-item>
+      </v-tabs-items>
+    </v-card>
   </div>
 </template>
 <script>
 import axios from "axios";
-import { csrfToken } from "rails-ujs";
-axios.defaults.headers.common["X-CSRF-TOKEN"] = csrfToken();
 
 import UserShow from "../../components/User/UserShow.vue";
-import PostsIndex from "../../components/Post/PostsIndex.vue";
+import PostsIndexImage from "../../components/Post/PostsIndexImage.vue";
 
 export default {
   components: {
     UserShow,
-    PostsIndex,
+    PostsIndexImage,
   },
   data() {
     return {
@@ -25,14 +42,18 @@ export default {
         },
         posts: [],
       },
+      tabsKey: null,
+      tabasItem: ["投稿", "いいね"],
+      likesPosts: [],
     };
   },
-  created() {
+  mounted() {
     this.updateUser();
+    this.updateLikes();
   },
   watch: { $route: "updateUser" },
   methods: {
-    setUser: async function () {
+    async setUser() {
       const res = await axios.get(
         `/api/v1/users/${this.$route.params.id}.json`
       );
@@ -41,6 +62,17 @@ export default {
     updateUser() {
       this.setUser().then((result) => {
         this.user = result;
+      });
+    },
+    async setUserLikes() {
+      const res = await axios.get(
+        `/api/v1/likes/${this.$route.params.id}/posts.json`
+      );
+      return res.data;
+    },
+    updateLikes() {
+      this.setUserLikes().then((result) => {
+        this.likesPosts = result;
       });
     },
   },
