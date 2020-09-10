135a0b143f2929126cae99df7fd17b998915470a
omtmshd <omt1031mshd@gmail.com>
2020-09-10 16:49:13 +0000

    webpack修正

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -2,9 +2,6 @@ import Vue from 'vue'
 
 import Vuetify from "vuetify";
 import "vuetify/dist/vuetify.min.css";
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
-import "@mdi/font/css/materialdesignicons.css";
-
 
 import App from '../pages/app.vue'
 
