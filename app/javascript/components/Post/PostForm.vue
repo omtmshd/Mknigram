df35c0dbbab201b83faed32f7cd699132039748d
omtmshd <67169292+omtmshd@users.noreply.github.com>
2020-09-11 12:41:51 +0900

    refactor: category form 修正 (#80)

--- app/javascript/components/Post/PostForm.vue
+++ app/javascript/components/Post/PostForm.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-    <v-card class="mx-auto" max-width="400">
+    <v-card class="mx-auto">
       <v-container>
         <v-form @submit.prevent="$emit('submit')">
           <div v-if="errors.length != 0">
@@ -10,101 +10,79 @@
               </li>
             </ul>
           </div>
-          <v-card class="mx-auto" width="320">
-            <v-img
-              :src="image"
-              aspect-ratio="1.2"
-              class="white--text align-end"
-              gradient="to bottom, rgba(0,0,0,0), rgba(0,0,0,.3)"
-            >
-              <v-card-title v-text="post.title"></v-card-title>
-            </v-img>
-            <v-breadcrumbs :items="post.categories" text="name">
-              <template v-slot:item="{ item }">
-                <v-breadcrumbs-item>{{ item.name }}</v-breadcrumbs-item>
-              </template>
-            </v-breadcrumbs>
-            <v-card-text>{{ post.body }}</v-card-text>
-          </v-card>
           <v-row justify="center">
-            <v-col cols="9">
-              <v-file-input
-                dense
-                @change="selectedFile"
-                label="写真"
-                prepend-icon="mdi-camera"
-                chips
-                :clearable="false"
-                color="#FDD835"
-              ></v-file-input>
+            <v-col cols="12" md="6" lg="6" xl="6">
+              <v-card class="mx-auto" width="320">
+                <v-img
+                  :src="image"
+                  aspect-ratio="1.2"
+                  class="white--text align-end"
+                  gradient="to bottom, rgba(0,0,0,0), rgba(0,0,0,.3)"
+                >
+                  <v-card-title v-text="post.title"></v-card-title>
+                </v-img>
+                <v-breadcrumbs :items="post.categories" text="name">
+                  <template v-slot:item="{ item }">
+                    <v-breadcrumbs-item>{{ item.name }}</v-breadcrumbs-item>
+                  </template>
+                </v-breadcrumbs>
+                <v-card-text>{{ post.body }}</v-card-text>
+              </v-card>
+              <v-row justify="center">
+                <v-col cols="9">
+                  <v-file-input
+                    dense
+                    @change="selectedFile"
+                    label="写真"
+                    prepend-icon="mdi-camera"
+                    chips
+                    :clearable="false"
+                    color="#FDD835"
+                  ></v-file-input>
+                  <v-text-field
+                    counter="20"
+                    dense
+                    v-model="post.title"
+                    label="タイトル"
+                    required
+                    color="#FDD835"
+                    prepend-icon="mdi-chef-hat"
+                  ></v-text-field>
 
-              <v-text-field
-                counter="20"
-                dense
-                v-model="post.title"
-                label="タイトル"
-                required
-                color="#FDD835"
-                prepend-icon="mdi-chef-hat"
-              ></v-text-field>
-
-              <v-textarea
-                counter="250"
-                dense
-                v-model="post.body"
-                label="レシピ"
-                required
-                rows="2"
-                color="#FDD835"
-                prepend-icon="mdi-silverware"
-              ></v-textarea>
-
-              <v-select
-                dense
-                solo
-                v-model="post.categories[0]"
-                :items="parents"
-                item-text="name"
-                item-value="id"
-                return-object
-                @change="selectParents"
-                label="メインカテゴリー"
-                color="#FDD835"
-                item-color="#FDD835"
-                prepend-icon="mdi-menu-right"
-              />
-              <v-select
-                dense
-                solo
-                v-model="post.categories[1]"
-                :items="children"
-                item-text="name"
-                item-value="id"
-                return-object
-                no-data-text="カテゴリーがありません"
-                @change="selectChildren"
-                label="サブカテゴリー"
-                color="#FDD835"
-                item-color="#FDD835"
-                prepend-icon="mdi-menu-right"
-              />
-              <v-select
-                dense
-                solo
-                v-model="post.categories[2]"
-                :items="grandchildren"
-                item-text="name"
-                item-value="id"
-                return-object
-                no-data-text="カテゴリーがありません"
-                label="サブカテゴリー2"
-                color="#FDD835"
-                item-color="#FDD835"
-                prepend-icon="mdi-menu-right"
-              />
-              <v-row justify="end">
-                <v-col cols="4">
-                  <v-btn rounded color="#FFF176" class="mr-4" type="submit">投稿する</v-btn>
+                  <v-textarea
+                    counter="250"
+                    dense
+                    v-model="post.body"
+                    label="レシピ"
+                    required
+                    rows="2"
+                    color="#FDD835"
+                    prepend-icon="mdi-silverware"
+                  ></v-textarea>
+                </v-col>
+              </v-row>
+            </v-col>
+            <v-col cols="12" md="6" lg="6" xl="6">
+              <v-row justify="center">
+                <v-col cols="9">
+                  <v-treeview
+                    :items="categoriesData"
+                    v-model="post.categories"
+                    selection-type="leaf"
+                    selected-color="#FDD835"
+                    dense
+                    activatable
+                    expand-icon="mdi-chevron-down"
+                    open-on-click
+                    return-object
+                    selectable
+                    transition
+                  ></v-treeview>
+                  <v-row justify="end" align="end">
+                    <v-col cols="4">
+                      <v-btn rounded color="#FFF176" class="mr-4" type="submit">投稿する</v-btn>
+                    </v-col>
+                  </v-row>
                 </v-col>
               </v-row>
             </v-col>
@@ -123,79 +101,42 @@ export default {
       title: "",
       body: "",
       post_image: "",
+      categories: [],
       category_ids: [],
-      categories: [
-        { id: "", name: "" },
-        { id: "", name: "" },
-        { id: "", name: "" },
-      ],
     },
     errors: "",
   },
   data() {
     return {
-      parents: [],
-      children: [],
-      grandchildren: [],
+      categoriesData: [],
       image: "",
     };
   },
   mounted() {
-    this.setParents();
-    if (this.post.id > 0) {
-      this.setChildren();
-      this.setGrandchildren();
-    }
+    this.getCategories();
   },
   updated() {
     // 編集画面で画像をセット
-    if (this.post.id > 0 && this.image !== this.post.post_image.url) {
+    if (this.post.id > 0 && this.image === "") {
       this.image = this.post.post_image.url;
     }
   },
   methods: {
     // ファイルを代入
     selectedFile(e) {
-      this.post.post_image = e;
-      this.image = window.URL.createObjectURL(e);
+      if (!e) {
+        this.post.post_image = null;
+        this.image = null;
+      } else {
+        this.post.post_image = e;
+        this.image = window.URL.createObjectURL(e);
+      }
       this.$emit("change-file");
     },
-    // 第1層カテゴリー取得
-    setParents() {
-      axios
-        .get("/api/v1/categories/parents")
-        .then((response) => (this.parents = response.data));
-    },
-    // 子カテゴリー取得
-    setChildren() {
-      if (this.post.categories[0] !== null) {
-        axios
-          .get(`/api/v1/categories/${this.post.categories[0].id}/children`)
-          .then((response) => (this.children = response.data));
-      }
-    },
-    // 孫カテゴリー取得
-    setGrandchildren() {
-      if (this.post.categories[1] !== null) {
-        axios
-          .get(`/api/v1/categories/${this.post.categories[1].id}/children`)
-          .then((response) => (this.grandchildren = response.data));
-      }
-    },
-    resetChildren() {
-      this.post.categories[1] = { id: "", name: "" };
-    },
-    resetGrandchildren() {
-      this.post.categories[2] = { id: "", name: "" };
-    },
-    selectParents() {
-      this.setChildren();
-      this.resetChildren();
-      this.resetGrandchildren();
-    },
-    selectChildren() {
-      this.setGrandchildren();
-      this.resetGrandchildren();
+    getCategories() {
+      axios.get("/api/v1/categories").then(({ data }) => {
+        this.categoriesData = data;
+      });
     },
   },
 };
--- app/javascript/pages/posts/PostsEditPage.vue
+++ app/javascript/pages/posts/PostsEditPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <post-form :post="post" :errors="errors" @submit="updatePost" @change-file="changeFile()"></post-form>
+  <post-form :post="post" :errors="errors" @submit="updatePost" @change-file="changeFile"></post-form>
 </template>
 
 <script>
@@ -15,36 +15,26 @@ export default {
         title: "",
         body: "",
         post_image: String,
+        categories: [],
         category_ids: [],
-        categories: [
-          { id: "", name: "" },
-          { id: "", name: "" },
-          { id: "", name: "" },
-        ],
       },
       errors: "",
       fileChange: false,
     };
   },
-  mounted() {
+  created() {
     this.setPost();
+    this.post.categories.forEach((category, index) => {
+      this.post.category_ids.push(category.id);
+    });
   },
   methods: {
     setPost() {
       axios
         .get(`/api/v1/posts/${this.$route.params.id}`)
         .then((response) => (this.post = response.data));
-      if (this.post.categories[0] != true) {
-        this.post.categories[0] = { id: "", name: "" };
-        if (this.post.categories[1] != true) {
-          this.post.categories[1] = { id: "", name: "" };
-          if (this.post.categories[2] != true) {
-            this.post.categories[2] = { id: "", name: "" };
-          }
-        }
-      }
     },
-    chageFile() {
+    changeFile() {
       this.fileChange = true;
     },
     updatePost() {
@@ -55,32 +45,9 @@ export default {
       if (this.fileChange === true) {
         formData.append("post[post_image]", this.post.post_image);
       }
-      // メインが選択済みである場合、代入
-      if (this.post.categories[0].id > 0) {
-        this.post.category_ids = [this.post.categories[0].id];
-        // サブのidがメインより大きい場合、代入
-        if (this.post.categories[1].id > this.post.categories[0].id) {
-          this.post.category_ids = [
-            this.post.categories[0].id,
-            this.post.categories[1].id,
-          ];
-          // サブ2のidがサブより大きい場合、代入
-          if (this.post.categories[2].id > this.post.categories[1].id) {
-            this.post.category_ids = [
-              this.post.categories[0].id,
-              this.post.categories[1].id,
-              this.post.categories[2].id,
-            ];
-          }
-        }
-        // 配列をまとめる
-        this.post.category_ids.forEach((category_id, index) => {
-          formData.append("post[category_ids][]", category_id);
-        });
-        // 有効なデータが無い場合、空配列
-      } else {
-        formData.append("post[category_ids][]", []);
-      }
+      this.post.categories.forEach((category, index) => {
+        formData.append("post[category_ids][]", category.id);
+      });
       axios
         .patch(`/api/v1/posts/${this.post.id}`, formData, {
           headers: {
