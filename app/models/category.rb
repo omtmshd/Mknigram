d7ba748465b57ccc27622219ce6822e21a953214
omtmshd <omt1031mshd@gmail.com>
2020-09-16 20:58:24 +0900

    refactor: カテゴリー検索修正

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,6 +5,16 @@ class Category < ApplicationRecord
   default_scope -> { order(:id) }
 
   def posts_count
-    self.posts.count
+    # 子カテゴリーがある場合
+    if self.children?
+      # 子カテゴリー、孫カテゴリーのPostを返す
+      return Post.where(id: PostCategory.where(category_id: self.descendant_ids).pluck(:post_id)).count
+
+      子カテゴリーがない場合
+    else
+      # 自身のPostレコードを返す
+      return self.posts.count
+    end
   end
+
 end
